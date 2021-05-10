07d7946a1587925ba0a529437e66f7453cfefc82
Frens1999 <frenskadiasi@gmail.com>
2021-05-10 23:22:19 +0200

    Add button to navigate on add-product

--- src/app/pages/products/containers/product/products.component.ts
+++ src/app/pages/products/containers/product/products.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {products, ProductService} from '../../../../core/services/product.service';
 import {DataSource} from '@angular/cdk/collections';
 import {Product} from '../../../../core/models/product.model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -12,7 +13,8 @@ export class ProductsComponent implements OnInit {
   displayedColumns: string[] = ['id', 'name', 'description', 'price', 'lowStock', 'optimalStock', 'stock', 'stock_type', 'barcode'];
   dataSource: any;
 
-  constructor(private productsService: ProductService) {
+  constructor(private productsService: ProductService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
@@ -22,5 +24,8 @@ export class ProductsComponent implements OnInit {
 
   onClickedRow(): void {
     console.log('hello');
+  }
+  addItem(): void {
+    this.router.navigateByUrl('dashboard/products/add');
   }
 }
--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -4,18 +4,23 @@ import {ProductsComponent} from './containers/product/products.component';
 import {RouterModule, Routes} from '@angular/router';
 import {MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatButtonModule} from '@angular/material/button';
+import {AddProductComponent} from './containers/product/add-product/add-product.component';
 
-const routes: Routes = [{path: '', component: ProductsComponent}];
+const routes: Routes = [{
+  path: '', component: ProductsComponent}];
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    AddProductComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     MatTableModule,
     MatPaginatorModule,
+    MatButtonModule,
 
   ]
 })
