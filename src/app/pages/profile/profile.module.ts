59699c02dbc741f14920d7d5c5ed65136f1d5f10
fiorelozere <fjorelo11@gmail.com>
2021-05-20 01:18:49 +0200

    profile page bug fixes

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -2,15 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProfileComponent} from './containers/profile/profile.component';
 import {RouterModule, Routes} from '@angular/router';
-import {MatCardModule} from "@angular/material/card";
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 
-const routes: Routes = [{path: '', component: ProfileComponent}];
+const routes: Routes = [{path: '', component: ProfileComponent, data: {animation: 'Profile'}}];
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ const routes: Routes = [{path: '', component: ProfileComponent}];
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatProgressSpinnerModule
   ]
 })
 export class ProfileModule {
