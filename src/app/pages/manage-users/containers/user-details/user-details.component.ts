f1da5096cfbe52be98989c38137f9dda8c69bfd7
Enea Jahollari <jahollarienea14@gmail.com>
2021-05-23 00:53:06 +0200

    Added fadeIn animation in user details page

--- src/app/pages/manage-users/containers/user-details/user-details.component.ts
+++ src/app/pages/manage-users/containers/user-details/user-details.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UsersService } from '@core/services/users.service';
+import { fadeIn } from '@app/animations/fadeIn.animation';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.scss']
+  styleUrls: ['./user-details.component.scss'],
+  animations: [ fadeIn ]
 })
 export class UserDetailsComponent {
 
