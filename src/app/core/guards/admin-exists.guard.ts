965e2db14805e6758674ff2cbd5e698d357fe7a0
Enea Jahollari <jahollarienea14@gmail.com>
2021-10-30 12:01:46 +0200

    Added screenshots

--- src/app/core/guards/admin-exists.guard.ts
+++ src/app/core/guards/admin-exists.guard.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { AuthService } from '@core/services/auth.service';
-import { map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AdminExistsGuard implements CanActivate {
 
-  constructor(private auth: AuthService, private router: Router) {}
+  constructor(private auth: AuthService, private router: Router) { }
 
   canActivate(): Observable<boolean> {
     return this.auth.adminExists$.pipe(
-      map(({ exists }) => exists),
-      tap(exists => {
+      map(({ exists }) => {
         if (!exists) {
           this.router.navigateByUrl('/auth/createAdmin');
         }
+        return exists;
       })
     );
   }
